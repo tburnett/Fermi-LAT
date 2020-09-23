ae5d24c3992bc79f5b73d27290fd165ad07c3531
tburnett <tburnett@uw.edu>
2020-09-23 05:38:08 -0700

    format fix

--- utilities/healpix.py
+++ utilities/healpix.py
@@ -181,30 +181,40 @@ class HPcube(HPmap):
         return cls(spectral_cube, energies, unit=unit) 
 
 
-    def to_FITS(self, outfile, overwrite=True):
+    def to_FITS(self, outfile, 
+            vector_format:'if True, one-column format'=False, 
+            overwrite=True):
 
         def spectral_table(self):
-            array = np.vstack([x.map for x in self]).T
+            
             el = self.energies
-            column = fits.Column(name='spectra', format=f'{len(el)}E',
-                        unit=getattr(self, 'unit',''), array=array)
-            table = fits.BinTableHDU.from_columns([column])
+
+            if vector_format:
+                columns = [fits.Column(name='spectra', format=f'{len(el)}E',
+                            unit=getattr(self, 'unit',''), 
+                            array=np.vstack([x.map for x in self]).T)
+                            ]
+            else:
+                columns = [fits.Column(name=f'CHANNEL{i:02d}', format='E', unit='', array=plane.map)
+                    for i, plane in enumerate(self)]
+                
+            table = fits.BinTableHDU.from_columns(columns)
             table.name = 'SKYMAP' 
             # add HEALPix and energy info to the header 
             nside = self.nside
 
             emin, deltae= el[0], np.log(el[1]/el[0])
             cards = [fits.Card(*pars) for pars in [ 
-                    ('FIRSTPIX',  0,             'First pixel (0 based)'),
-                    ('LASTPIX',  array.shape[0], 'Last pixel (0 based)'),
-                    ('MAPTYPE'   'Fullsky'   ),
+                    ('FIRSTPIX', 0,             'First pixel (0 based)'),
+                    ('LASTPIX',  12*nside**2, 'Last pixel (0 based)'),
+                    ('MAPTYPE',  'Fullsky' , ''  ),
                     ('PIXTYPE',  'HEALPIX',      'Pixel algorithm',),
                     ('ORDERING', 'RING',         'Ordering scheme'),
-                    ('NSIDE' ,    self.nside,    'Resolution Parameter'),
-                    ('ORDER',     int(np.log2(self.nside)),   'redundant'),
+                    ('NSIDE' ,   nside,    'Resolution Parameter'),
+                    ('ORDER',    int(np.log2(nside)),   'redundant'),
                     ('INDXSCHM', 'IMPLICIT' ,''),
                     ('OBJECT' ,  'FULLSKY', ''),
-                    ('NRBINS',   array.shape[1], 'Number of energy bins'),
+                    ('NRBINS',   len(el),      'Number of energy bins'),
                     ('COORDSYS', 'GAL', ''),
                     ('EMIN',     emin,           'Minimum energy'  ),
                     ('DELTAE',   deltae,         'Step in energy (log)'),
@@ -214,13 +224,18 @@ class HPcube(HPmap):
             return table
 
         def energy_table(self):
-            column= fits.Column( name='MeV', format='E', unit='MeV', array=self.energies)
+            column= fits.Column( name='energy', format='E', unit='MeV', array=self.energies)
             table = fits.BinTableHDU.from_columns([column])
             table.name='ENERGIES'
             return table
 
+        def primary(self):
+            cards = [fits.Card('COMMENT', 'Written by utilities/healpix.py '),
+                    ]
+            return fits.PrimaryHDU(header=fits.header.Header(cards))
+
         def hdu_list(self):
-            return [ fits.PrimaryHDU(header=None),  #primary
+            return [ primary(self),  #primary
                     spectral_table(self),           # this table
                     energy_table(self),
                 ]
@@ -228,7 +243,8 @@ class HPcube(HPmap):
         hdus = hdu_list(self)
 
         fits.HDUList(hdus).writeto(outfile, overwrite=overwrite)
-        print( f'\nwrote FITS Skymap file, nside={self.nside}, {len(self.energies)} energies, to {outfile}')
+        print( f'\nwrote {"vector format" if vector_format else ""} '\
+            f'FITS Skymap file, nside={self.nside}, {len(self.energies)} energies, to {outfile}')
 
 
     # def ratio(self, other, skydir, energy, label=''):
@@ -270,7 +286,7 @@ def ait_plot(mapable,
         pars=[],
         label='',        
         title='',
-        fig=None, 
+        fig=None, ax=None,
         pixelsize:'pixel size in deg'=1, 
         projection='aitoff',
         cmap='jet', 
@@ -299,9 +315,13 @@ def ait_plot(mapable,
     dirs = SkyDir.from_galactic(Lon, Lat)
     arr = mapable(dirs, *np.atleast_1d(pars))
 
-    fig = plt.figure(figsize=(12,5)) if fig is None else fig
-    # this needs to be more flexible
-    ax = fig.add_subplot(axes_pos, projection=projection, **axes_kw)
+    if ax:
+        fig = ax.figure
+        assert ax.__class____name__=='AitoffAxesSubplot'
+    else:
+        fig = plt.figure(figsize=(12,5)) if fig is None else fig
+        # this needs to be more flexible
+        ax = fig.add_subplot(axes_pos, projection=projection, **axes_kw)
 
     # reverse longitude sign here for display
 
@@ -336,3 +356,116 @@ def ait_multiplot(mapable,
 
     fig.tight_layout()
     if title: fig.suptitle(title, fontsize=16);
+
+
+class Polyfit(object):
+    """ Manage a log polynomral fit to each pixel
+    """
+    def __init__(self, 
+        cubefile, sigsfile, start=0, stop=8, deg=2,limits=(0.5,25)):
+        """
+        """
+        
+        m = HPcube.from_FITS(cubefile)
+        msig = HPcube.from_FITS(sigsfile)
+        self.nside= m.nside
+        self.limits=limits
+        
+        meas = np.array([m[i].map for i in range(8)])
+        sig  = np.array([msig[i].map for i in range(8)])
+
+        self.planes = np.array(range(start,stop)) # plane numbers
+        self.values = meas[start:,:]
+        weights = 100./sig[start:,:] #from percent
+        self.wtmean = weights.mean(axis=1)
+
+        self.fit, self.residuals, self.rank, self.svals, self.rcond =\
+            np.polyfit(self.planes,self.values, deg=deg, full=True, w=self.wtmean)
+            
+        labels= 'intercept slope curvature'.split()   
+        
+    def __getitem__(self, i):
+        return self.fit[i]
+
+    def energy_index(self, energy):
+        """convert energy in MeV to correspond to np.logspace(2.125, 3.875, 8)
+        """
+        energy = np.atleast_1d(energy)
+        return 4*np.log10(energy/100)-0.5
+    
+#     def ait_plots(self):
+#         self.hpfit=[healpix_map.HParray(labels[deg-i], self.fit[i,:]) for i in range(deg,-1,-1)]
+#         healpix_map.multi_ait(self.hpfit, cmap=plt.get_cmap('jet'),  grid_color='grey')
+
+    def __call__(self, 
+                coord: 'SkyDir', 
+                energy:'energies in MeV',
+                factor_cap=25,
+                
+                )->'interpolated list of factors':
+
+        """
+        Implements the sky function using the logparabola fit
+        Note that either arg can be multivalued, but not both.
+        """
+        energy = np.atleast_1d(energy)
+        pix = np.atleast_1d(coord.to_healpix(self.nside) )        
+        x = self.energy_index(energy)   
+        fit= self.fit[:,pix]; 
+        xx= np.vstack([x**2, x, np.ones(len(x))] )
+        ret = np.matmul(fit.T, xx).T
+        ret = np.clip(ret, *self.limits)
+        return ret[0] if ret.shape[0]==1 else ret
+
+    def parabolic_fit(self, 
+            x:'energy index', 
+            pix:'pixel index'):
+        """Evaluate the parabolic fit in energy index
+        """
+        # if not hasattr(x, '__iter__'):
+        #     x = np.array([x])
+        x = np.atleast_1d(x)
+        fit= self.fit[:,pix]; 
+        t =fit.reshape(3,1)
+        return ( t * np.vstack([x**2, x, np.ones(len(x))] )).sum(axis=0)
+    
+    def ang2pix(self, glon, glat):
+        return healpy.ang2pix(64, glon, glat, lonlat=True)
+        
+    def get_fit(self, pix):
+             
+        y = self.values[:,pix]
+        yerr = 1/self.wtmean
+        fn = lambda xx : self.parabolic_fit(xx, pix)
+        return y, yerr, fn
+    
+    def plot_fit(self, glon, glat, ax=None, axis_labels=True):
+
+        pix = self.ang2pix(glon, glat)
+        y, yerr, fn = self.get_fit(pix)
+
+        fig, ax =plt.subplots() if ax is None else (ax.figure, ax)
+        npl = len(self.planes)
+        xx = np.linspace(self.planes[0]-0.5,self.planes[-1]+0.5,2*npl+1)
+
+        ax.errorbar(self.planes, y, yerr=yerr, fmt='o', ms=8, label='measured values' if axis_labels else '');
+        ax.plot(xx, fn(xx), '-', lw=2, label='parabolic fit' if axis_labels else '');
+        ax.text(0.05,0.9,'({:3.0f}, {:+2.0f})'.format(glon, glat), transform=ax.transAxes)
+        if axis_labels:
+            ax.set(ylabel='flux factor', xlabel='Energy (GeV)')
+            ax.set(xticks=[-0.5, 3.5, 7.5], xticklabels='0.1 1.0 10'.split())
+            ax.legend()
+        else:
+            ax.set_xticks(self.planes[::2])  
+        ax.axhline(1.0, color='grey')
+        ax.grid(alpha=0.3)
+        
+    def multiplot(self, glons, glats, grid_shape=(4,5), title=''):
+ 
+        fig, axx = plt.subplots(grid_shape[0],grid_shape[1], figsize=(12,12), sharex=True, sharey=True,
+                            gridspec_kw=dict(left=0.05, right = 0.95,top=0.95, wspace=0, hspace=0)  )
+        for glon, glat, ax in zip(glons, glats, axx.flatten()):
+            self.plot_fit( glon, glat, ax, axis_labels=False)
+        fig.suptitle(title, fontsize=16); 
+        fig.set_facecolor('white')
+           
